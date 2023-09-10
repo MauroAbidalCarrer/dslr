94f947b18550e5e1537f5f610ffc9078e0a1b254
Mauro ABIDAL CARRER <mauroabidal@yahoo.fr>
2023-09-10 19:54:23 +0200

    Added histograms

--- visualization.py
+++ visualization.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from load_dataset import get_expected_output_training_data, get_input_data, TRAIN_DATASET_PATH
 
@@ -8,18 +9,23 @@ inputs = get_input_data(TRAIN_DATASET_PATH)
 nb_features = inputs.shape[0]
 
 # Creating scatterplots for the subset of nb_inputs using the available rows
-fig, axes = plt.subplots(nb_features, nb_features, figsize=(24, 24))
+SIZE = 35
+fig, axes = plt.subplots(nb_features, nb_features, figsize=(SIZE, SIZE))
 fig.tight_layout(pad=3.0)
 
-for i in range(nb_features):
-    for j in range(nb_features):
-        if i != j:
-            axes[i, j].scatter(inputs[i, :], inputs[j, :], c=subset_expected_outputs, cmap='viridis', alpha=0.5, s=5)
-            axes[i, j].set_xlabel(f"Feature {i}")
-            axes[i, j].set_ylabel(f"Feature {j}")
+for x in range(nb_features):
+    for y in range(nb_features):
+        if x != y:
+            axes[x, y].scatter(inputs[x, :], inputs[y, :], c=subset_expected_outputs, cmap='viridis', alpha=0.5, s=5)
+            if y == 0:
+                axes[x, y].set_xlabel(f"Feature {x}")
+            if x == 0:
+                axes[x, y].set_ylabel(f"Feature {y}")
         else:
-            axes[i, j].text(0.5, 0.5, f"Feature {i}", ha='center', va='center')
-        axes[i, j].set_xticks([])
-        axes[i, j].set_yticks([])
+            data_by_house = [inputs[x, subset_expected_outputs == k] for k in range(4)]
+            axes[x, y].hist(data_by_house, bins=30, stacked=True, color=plt.cm.viridis(np.linspace(0, 1, 4)))
+            # axes[x, y].set_title(f"Feature {x}")
+        axes[x, y].set_xticks([])
+        axes[x, y].set_yticks([])
 
 plt.show()
