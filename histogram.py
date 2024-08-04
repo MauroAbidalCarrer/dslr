1360e813404088cb9a394a0e8439587afc453176
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-08-04 20:19:36 +0200

    FIx: Added a missing feature

--- histogram.py
+++ histogram.py
@@ -2,11 +2,14 @@ from load_dataset import get_training_dataset
 import numpy as np
 import matplotlib.pyplot as plt
 
-MOST_HOMOGENEOUS_FEATURE_INDEX = 11
+MOST_HOMOGENEOUS_FEATURE_INDICES = [1, 11]
 
 inputs, outputs = get_training_dataset()
 
-data_by_house = [inputs[outputs == k, MOST_HOMOGENEOUS_FEATURE_INDEX] for k in range(4)]
-plt.hist(data_by_house, bins=30, stacked=True, color=plt.cm.viridis(np.linspace(0, 1, 4)))
+fig, axs = plt.subplots(nrows=2)
+for feat_idx, ax in zip(MOST_HOMOGENEOUS_FEATURE_INDICES, axs):
+    data_by_house = [inputs[outputs == k, feat_idx] for k in range(4)]
+    ax.hist(data_by_house, bins=30, stacked=True, color=plt.cm.viridis(np.linspace(0, 1, 4)))
+    ax.set_title(f"feature: {feat_idx}")
 
 plt.show()
