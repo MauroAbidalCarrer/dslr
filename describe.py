839697a0f55a89e17c4ba7390dba7f8012153bb5
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-08-04 20:21:17 +0200

    Fix: Fixed features takens, and added feature names.

--- describe.py
+++ describe.py
@@ -3,20 +3,78 @@ import numpy as np
 from tabulate import tabulate
 from load_dataset import get_training_dataset
 
-def describe_column(column):
+
+ALL_FEATURES = [
+    "Index",
+    "Hogwarts House",
+    "First Name",
+    "Last Name",
+    "Birthday",
+    "Best Hand",
+    "Arithmancy",
+    "Astronomy",
+    "Herbology",
+    "Defense Against the Dark Arts",
+    "Divination",
+    "Muggle Studies",
+    "Ancient Runes",
+    "History of Magic",
+    "Transfiguration",
+    "Potions",
+    "Care of Magical Creatures",
+    "Charms",
+    "Flying"
+]
+
+FEATURE_NAMES = [
+    "Best Hand",
+    "Arithmancy",
+    "Astronomy",
+    "Herbology",
+    "Defense Against the Dark Arts",
+    "Divination",
+    "Muggle Studies",
+    "Ancient Runes",
+    "History of Magic",
+    "Transfiguration",
+    "Potions",
+    "Care of Magical Creatures",
+    "Charms",
+    "Flying",
+    
+    # # "Hogwarts House",
+    # "Best Hand",
+    # "Arithmancy",
+    # "Astronomy",
+    # "Herbology",
+    # "Defense Against the Dark Arts",
+    # "Divination",
+    # "Muggle Studies",
+    # "Ancient Runes",
+    # "History of Magic",
+    # "Transfiguration",
+    # "Potions",
+    # "Care of Magical Creatures",
+    # "Charms",
+    # "Flying"
+]
+
+def describe_column(column: np.ndarray):
     """
     Retruns an array of characteristics of the column:
     Count, Mean, Std, Min, 25%, 50%, 75%, Max 
     """
-    sorted_column = np.sort(column)
-    count = len(column)
-    mean = sum(column) / count
+    notna_rows = ~np.isnan(column)
+    count = np.count_nonzero(notna_rows)
+    mean = np.nansum(column, ) / count
     squared_differences = [(value - mean) ** 2 for value in column]
     # I don't really understand why but you need to devide by count - 1 instead of just count.
-    std_deviation = math.sqrt(sum(squared_differences) / (count - 1)) 
+    std_deviation = math.sqrt(np.nansum(squared_differences) / (count - 1)) 
     # print("float count:", count, ", int count:", int(count))
 
-    def median(sorted_values):
+    sorted_column = np.sort(column[notna_rows])
+    def median(sorted_values, ):
+
         middle_index = len(sorted_values) // 2
         if len(sorted_values) % 2 == 1:
             return sorted_values[middle_index] 
@@ -27,9 +85,9 @@ def describe_column(column):
         mean,
         std_deviation,
         sorted_column[0],
-        median(sorted_column[:count // 2]),
-        median(sorted_column),
-        median(sorted_column[count // 2 + count % 2:]),
+        median(sorted_column[:count // 2 + count % 2]), #median(sorted_column[:count // 2]),
+        median(sorted_column), #median(sorted_column),
+        median(sorted_column[count // 2:]), #median(sorted_column[count // 2:]),
         sorted_column[int(count) - 1],
     ]
 
@@ -39,10 +97,11 @@ def describe_all(data):
     """
     all_descriptions = [describe_column(data[:, col]) for col in range(data.shape[1])]
     headers = ["Count", "Mean", "Std", "Min", "25%", "50%", "75%", "Max"]
-    col_names = [f"Feature {i+1}" for i in range(data.shape[1])]
+    col_names = FEATURE_NAMES #[f"Feature {i+1}" for i in range(data.shape[1])]
     table = tabulate(all_descriptions, headers=headers, showindex=col_names, tablefmt='grid')
     print(table)
 
-np.set_printoptions(precision=2)
-inputs, outputs = get_training_dataset()
-describe_all(inputs)
+if __name__ == "__main__":
+    # np.set_printoptions(precision=2)
+    inputs, outputs = get_training_dataset()
+    describe_all(inputs)
