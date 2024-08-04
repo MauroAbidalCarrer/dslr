ef9146b49159ee6326f82fbe85abf3ee0d68f87e
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2024-08-04 20:22:01 +0200

    Fix: Added missing feature loading (last feature), removed nan removal by default.

--- load_dataset.py
+++ load_dataset.py
@@ -13,12 +13,12 @@ def get_data(path):
         delimiter=',',
         dtype=float,
         skip_header=1,
-        usecols=[1] + list(range(5, 18)),
+        usecols=[1] + list(range(5, 19)),
         converters={1: house_converter, 5: hand_converter}
-        )
+    )
     
     # Keep(data[...]) any(.any) row(axis=1) that has no(~) nan elements(np.isnan(data))
-    data = data[~np.isnan(data).any(axis=1)]
+    # data = data[~np.isnan(data).any(axis=1)]
 
     return data
 
@@ -45,4 +45,8 @@ def get_training_dataset(path=TRAIN_DATASET_PATH):
 def get_training_data_for_model(path):
     inputs, outputs = get_training_dataset(path)
     inputs = inputs[:, [2, 3]]      #Only keep 2nd and 3thd feature columns
+    notnan_mask = ~np.isnan(inputs).any(axis=1)
+    inputs = inputs[notnan_mask]
+    outputs = outputs[notnan_mask]
+
     return inputs, outputs
